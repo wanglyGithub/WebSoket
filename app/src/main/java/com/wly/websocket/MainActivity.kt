0a3f9e908fbc8e66144ddd51e7dbc5c463e1ce73
wanglyGithub <wlytctw@163.com>
2021-04-17 17:22:06 +0800

    修改布局文件

--- app/src/main/java/com/wly/websocket/MainActivity.kt
+++ app/src/main/java/com/wly/websocket/MainActivity.kt
@@ -2,11 +2,19 @@ package com.wly.websocket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.wly.websocket.example.OKHttpWebSocketTest
 
 class MainActivity : AppCompatActivity() {
+    private var webSocketTest: OKHttpWebSocketTest?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        webSocketTest = OKHttpWebSocketTest()
+    }
+
+    fun onStart(view: View) {
+        webSocketTest?.init()
     }
 }
