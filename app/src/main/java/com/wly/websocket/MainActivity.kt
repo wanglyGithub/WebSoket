1d956f0c71ed78dc7b95daf9fd3fcb224fe2928e
wanglyGithub <wlytctw@163.com>
2021-04-26 17:40:28 +0800

    webSocketLib Library 库 二期验证使用方式提交

--- app/src/main/java/com/wly/websocket/MainActivity.kt
+++ app/src/main/java/com/wly/websocket/MainActivity.kt
@@ -4,17 +4,50 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.wly.websocket.example.OKHttpWebSocketTest
+import com.wly.websocketlib.WSLinkManager
+import com.wly.websocketlib.listener.MainThreadWSListener
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.WebSocket
 
 class MainActivity : AppCompatActivity() {
-    private var webSocketTest: OKHttpWebSocketTest?= null
+    private var webSocketTest: OKHttpWebSocketTest? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        webSocketTest = OKHttpWebSocketTest()
+        webSocketTest = OKHttpWebSocketTest(this)
     }
 
     fun onStart(view: View) {
         webSocketTest?.init()
+
+
+    }
+
+    fun test2() {
+
+        val client = OkHttpClient.Builder().build()
+
+        val wsLinkManager = WSLinkManager.Builder()
+            .socketUrl("")
+            .client(client)
+            .build()
+
+        wsLinkManager.setWebSocketListener(object : MainThreadWSListener {
+            override fun onOpen(webSocket: WebSocket, response: Response) {
+
+            }
+
+            override fun onClose(webSocket: WebSocket, code: Int, reason: String) {
+            }
+
+            override fun onMessage(webSocket: WebSocket, body: String) {
+            }
+
+            override fun onFailure(webSocket: WebSocket, throwable: Throwable) {
+            }
+
+        })
     }
 }
